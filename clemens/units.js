e9a2fa9947d293f58e4637fa1fb6774b2d79f4fc
Clemens Winter <clemenswinter1@gmail.com>
2022-12-14 02:01:52 -0800

    procgen plots

---
+++ clemens/units.js
@@ -0,0 +1,16 @@
+observation = {
+    "allies": [
+        { "x": 213.0, "y": 1223.0, "health": 1, "attack": 1, "production": 0, "storage": 0, "resources": 0, },
+        { "x": 897.0, "y": 304.0, "health": 12, "attack": 3, "production": 3, "storage": 3, "resourcees": 4 },
+        { "x": 194.0, "y": 72.0, "health": 1, "attack": 0, "production": 0, "storage": 1, "resources": 5 },
+    ],
+    "mineral-crystals": [
+        { "x": 977.0, "y": 289.0, "size": 4012 },
+        { "x": 201.0, "y": 236.0, "size": 3 },
+    ],
+    "enemies": [
+        { "x": 158.0, "y": 1321.0, "health": 1, "attack": 1, "production": 0, "storage": 0, "resources": 0, },
+    ],
+}
+
+actions = ["fire", "move_forward", "collect"]
